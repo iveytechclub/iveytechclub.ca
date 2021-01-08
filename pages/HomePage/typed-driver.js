ed0147445c2251fee3fd155832df08abaf4c03fe
Jeffrey Chen <44081961+JefffffC@users.noreply.github.com>
2021-01-07 21:27:58 -0500

    completed home page main hero

---
+++ pages/HomePage/typed-driver.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+  smartBackspace: true,
+  typeSpeed: 50,
+  backSpeed: 40,
+  startDelay: 1000,
+  backDelay: 1200,
+  loop: true,
+  fadeOutDelay: 200,
+  autoInsertCss: true,
+});
