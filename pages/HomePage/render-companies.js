3d4f0aa77d020e605020e95d666d28656f84405e
Jeffrey Chen <44081961+JefffffC@users.noreply.github.com>
2021-01-07 23:28:22 -0500

    completed landing page about section

---
+++ pages/HomePage/render-companies.js
@@ -0,0 +1,19 @@
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open("GET", "pages/HomePage/companies.json", true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
+window.addEventListener("load", function () {
+  loadJSON((response) => {
+    var companiesObject = JSON.parse(response);
+    console.log(companiesObject);
+  });
+});
